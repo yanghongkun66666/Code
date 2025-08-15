491f1840ab9cb5df402b93d65c9c13b1b077c414
yanghongkun666666 <1355616593@qq.com>
2025-08-15 16:42:43 +0800

    Implements merge intervals solution
    Implements the merge intervals algorithm to combine overlapping intervals into non-overlapping intervals.
    
    The solution sorts the intervals by their start times and then iterates through them, merging overlapping intervals into a single interval.

---
+++ src/leetcode/p56_mergeInterval/Main.java
@@ -0,0 +1,25 @@
+package leetcode.p56_mergeInterval;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int[][] intervals = new int[n][2];
+
+        for (int i = 0; i < n; ++i) {
+            intervals[i][0] = sc.nextInt();
+            intervals[i][1] = sc.nextInt();
+        }
+
+        Solution s = new Solution();
+        int[][] res = s.merge(intervals);
+
+        for (int[] interval : res) {
+            System.out.println(interval[0] + " " + interval[1]);
+        }
+    }
+}
