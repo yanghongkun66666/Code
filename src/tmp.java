8ecf934a44dcd456c29eb9e1499ec22fb0f9630e
yanghongkun666666 <1355616593@qq.com>
2025-09-04 23:26:16 +0800

    mcp server

--- src/tmp.java
+++ src/tmp.java
@@ -5,7 +5,18 @@ import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 public class tmp {
     public static void main(String[] args) {
-        Map<String, Integer> map = new HashMap<>();
-        map.get("hh");
+        //  https://github.com/mrkrsl/web-search-mcp
+        /*
+
+        {
+          "mcpServers": {
+            "web-search": {
+              "args": ["/path/to/web-search-mcp/dist/index.js"],
+              "command": "node"
+            }
+          }
+        }
+
+        */
     }
 }
