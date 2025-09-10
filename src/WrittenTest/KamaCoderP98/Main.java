1a8f6e3d63129b715084aa8829bc4f1aa416549d
yanghongkun666666 <1355616593@qq.com>
2025-09-11 00:18:00 +0800

    邻接表dfs

--- src/WrittenTest/KamaCoderP98/Main.java
+++ src/WrittenTest/KamaCoderP98/Main.java
@@ -22,22 +22,24 @@ public class Main {
             return;
         }
 
-         // 邻接矩阵写法
-         for (int i = 1; i <= n; ++i) {
-             if (graph.get(x).get(i) == 1) {
-                 path.add(i);
-                 dfs(graph, i, n);
-                 path.remove(path.size() - 1);
-             }
-         }
-
 //        //非边界条件
-//        for (int neighbor : graph.get(x)) {
-//            path.add(neighbor);
-//            dfs(graph, neighbor, n);
-//            path.remove(path.size() - 1); //走到这里说明当前路径中添加neighbor后，如果后续有结果已经存到res了，没结果就回退到这里了
-//            //该回溯一下尝试处理下一个neighbor了
+//        // 邻接矩阵写法
+//        for (int i = 1; i <= n; ++i) {
+//            if (graph.get(x).get(i) == 1) {
+//                path.add(i);
+//                dfs(graph, i, n);
+//                path.remove(path.size() - 1);
+//            }
 //        }
+
+        //非边界条件
+        for (int neighbor : graph.get(x)) {
+            path.add(neighbor);
+            dfs(graph, neighbor, n);
+            path.remove(path.size() - 1); //走到这里说明当前路径中添加neighbor后，
+            // 如果后续有结果已经存到res了，没结果就回退到这里了
+            //该回溯一下尝试处理下一个neighbor了
+        }
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -45,28 +47,48 @@ public class Main {
         int n = sc.nextInt(); //代表有几个节点
         int m = sc.nextInt(); //代表有几条边
 
-        //邻接矩阵  节点为1-n
+//        //邻接矩阵  节点为1-n
+//        List<List<Integer>> graph = new ArrayList<>(n + 1);
+//        // n + 1行，每一行是一个列表，邻接矩阵的话就要每一行是一个n + 1 长度的列表，然后每个元素都初始化为0
+//        for (int i = 0; i <= n + 1; i++) {
+//            //这是遍历每一行,为了初始化每一行为一个长度为n + 1的列表
+//            List<Integer> row = new ArrayList<>();
+//            for (int j = 0; j <= n; ++j) {
+//                row.add(0);
+//            }
+//            graph.add(row);
+//        }
+//        //至此邻接矩阵初始化完成
+//        //由于是有向图，因此邻接矩阵不是对称赋值的
+//        //m代表边的个数
+//        while (m != 0) {
+//            int x = sc.nextInt();
+//            int y = sc.nextInt(); //读取到 x->y
+//            graph.get(x).set(y, 1);  //注意这个语法
+//            m--;
+//        }
+        //至此邻接矩阵构建完成
+
+
+
+        //邻接表实现方式
         List<List<Integer>> graph = new ArrayList<>(n + 1);
-        // n + 1行，每一行是一个列表，邻接矩阵的话就要每一行是一个n + 1 长度的列表，然后每个元素都初始化为0
-        for (int i = 0; i <= n + 1; i++) {
-            //这是遍历每一行,为了初始化每一行为一个长度为n + 1的列表
-            List<Integer> row = new ArrayList<>();
-            for (int j = 0; j <= n; ++j) {
-                row.add(0);
-            }
-            graph.add(row);
+        //每一行看成是一个链表
+        for (int i = 1; i <= n; ++i) {
+            graph.add(new ArrayList<>());
+            //每行都是一个链表，注意跟邻接矩阵的区别
         }
-        //至此邻接矩阵初始化完成
 
-        //由于是有向图，因此邻接矩阵不是对称赋值的
-
-        //m代表边的个数
         while (m != 0) {
             int x = sc.nextInt();
-            int y = sc.nextInt(); //读取到 x->y
-            graph.get(x).set(y, 1);  //注意这个语法
+            int y = sc.nextInt();
+            graph.get(x).add(y);
             m--;
         }
+        //至此邻接表构建完毕
+
+
+
 
         path.add(1); //无论是什么路径一定是从1号节点开始遍历
         dfs(graph, 1, n); //开始遍历
